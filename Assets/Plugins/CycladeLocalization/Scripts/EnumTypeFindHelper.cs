0c69b974d27758f13fa7688ea7fa9838cfcaa444
Oleg Karlovskii <steadcst@gmail.com>
2023-12-15 03:52:26 +0300

    cache types

--- Assets/Plugins/CycladeLocalization/Scripts/EnumTypeFindHelper.cs
+++ Assets/Plugins/CycladeLocalization/Scripts/EnumTypeFindHelper.cs
@@ -6,7 +6,11 @@ namespace CycladeLocalization
 {
     public static class EnumTypeHelper
     {
-        public static Type FindLang() => CycladeHelpers.FindTypesWith(q => q.FullName == "CycladeLocalization.Definition.Language" && q.IsEnum).Single();
-        public static Type FindArea() => CycladeHelpers.FindTypesWith(q => q.FullName == "CycladeLocalization.Definition.Area" && q.IsEnum).Single();
+        private static Cache<Type> TypeLang = new(() => CycladeHelpers.FindTypesWith(q => q.FullName == "CycladeLocalization.Definition.Language" && q.IsEnum).Single());
+        private static Cache<Type> TypeArea = new(() => CycladeHelpers.FindTypesWith(q => q.FullName == "CycladeLocalization.Definition.Area" && q.IsEnum).Single());
+
+        public static Type FindLang() => TypeLang.Value;
+
+        public static Type FindArea() => TypeArea.Value;
     }
 }
